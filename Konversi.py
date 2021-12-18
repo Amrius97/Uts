692db0989444c80da3cad8fb8846b18be722dd4e
Amrius97 <93589444+Amrius97@users.noreply.github.com>
2021-12-18 14:44:03 +0700

    no 1

---
+++ Konversi.py
@@ -0,0 +1,18 @@
+Nilai = float(input("Masukan Nilai"))
+if Nilai >= 91 and Nilai < 100 :
+    print ("A")
+elif Nilai >= 81 and Nilai <= 90 :
+    print ("AB")
+elif Nilai >=71 and Nilai <= 80 :
+    print ("B")
+elif Nilai >= 61 and Nilai <= 70 :
+    print ("CD")
+elif Nilai >= 41 and Nilai <= 60 :
+    print ("C")
+elif Nilai >= 21 and Nilai <= 40 :
+    print ("D")
+elif Nilai > 0 and Nilai <= 20 :
+    print ("E")
+else :
+    print ("Nilai Tidak Diketahui")
+    
